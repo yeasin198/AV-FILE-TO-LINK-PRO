7701dfc2d596a9b5b47dc93d1cc443f388bcac0b
yeasin198 <ya2620831@gmail.com>
2025-09-24 01:14:01 +0600

    Update info.py
    Signed-off-by: yeasin198 <ya2620831@gmail.com>

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ QR_CODE = environ.get('QR_CODE', 'https://graph.org/file/6afb4093d5ec5c4176979.j
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")  # Verify success image
 AUTH_PICS = environ.get('AUTH_PICS', 'https://envs.sh/AwV.jpg')  # Auth step image
 PICS = environ.get('PICS', 'https://envs.sh/_pM.jpg')  # Default info image
-FILE_PIC = environ.get('FILE_PIC', 'https://i.ibb.co/bj4My0bW/photo-2025-07-21-02-15-21-7529360175656861700.jpg') # file image 
+FILE_PIC = environ.get('FILE_PIC', 'https://envs.sh/5ZJ.jpg') # file image 
 
 # 📝 File Captions
 FILE_CAPTION = environ.get('FILE_CAPTION', f"{script.CAPTION}")  # Caption for single file
