b239f1722f550dfdebdedfc344a73e6a645d2692
yeasin198 <ya2620831@gmail.com>
2025-09-24 02:01:11 +0600

    Update batch.py
    Signed-off-by: yeasin198 <ya2620831@gmail.com>

--- plugins/batch.py
+++ plugins/batch.py
@@ -53,11 +53,11 @@ def unpack_new_file_id(new_file_id: str):
 @Client.on_message(filters.command(['batch']) & filters.create(allowed))
 async def gen_link_batch(bot, message: Message):
     if " " not in message.text:
-        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/AV_BOTz_UPDATE/10 https://t.me/AV_BOTz_UPDATE/20</code>.")
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/HDFLIXMAX/389 https://t.me/HDFLIXMAX/418</code>.")
     
     links = message.text.strip().split(" ")
     if len(links) != 3:
-        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/AV_BOTz_UPDATE/10 https://t.me/AV_BOTz_UPDATE/20</code>.")
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/HDFLIXMAX/389 https://t.me/HDFLIXMAX/418</code>.")
     
     cmd, first, last = links
     regex = re.compile(r"(https://)?(t\.me|telegram\.me|telegram\.dog)/(c/)?([\d\w_]+)/(\d+)")
