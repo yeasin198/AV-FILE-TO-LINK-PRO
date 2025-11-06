d5e0cea8b4d3cd1791f71251d025563fb932dad5
yeasin198 <ya2620831@gmail.com>
2025-11-06 17:15:30 +0600

    Update support group contact in maintenance message
    Signed-off-by: yeasin198 <ya2620831@gmail.com>

--- plugins/Maintenance.py
+++ plugins/Maintenance.py
@@ -11,7 +11,7 @@ async def maintenance_checker(client, message: Message):
     chat_id = message.chat.id
     if MAINTENANCE_MODE and user_id not in BYPASS_IDS and chat_id not in BYPASS_IDS:
         await message.reply(
-            "🚧 **Bot is under Maintenance!**\n\nPlease try again later.\n\nSupport Group: @AV_SUPPORT_GROUP",
+            "🚧 **Bot is under Maintenance!**\n\nPlease try again later.\n\nSupport Group: @jdflixmax",
             quote=True
         )
         return
